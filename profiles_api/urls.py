27c8fa7448db6d6dad2c1eae69f1195aa5a5769e
MsNahid <m.nahidzaman@gmail.com>
2021-03-24 16:54:46 +0600

    Adde all

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('first-viewset', views.HelloViewSet, basename='first-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
